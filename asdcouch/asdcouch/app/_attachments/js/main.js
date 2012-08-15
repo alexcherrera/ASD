36a6e031f1da3564dee9c9b051c762674012e560
Alexander Herrera <alexcherrera@fullsail.edu>
2012-08-15 13:48:24 -0400

    Adding the data in the main.js file and working on the map.js file

--- asdcouch/asdcouch/app/_attachments/js/main.js
+++ asdcouch/asdcouch/app/_attachments/js/main.js
@@ -1,37 +1,27 @@
-$('#home').on('pageinit', function(){
-    //JSON Data Pull
-    $('#json').on('click', function (){
-        //$('#showJSON').empty();
-        //$('#showJson').empty();
-        $.ajax({
-            "url": "college-app/schedules1",
-            //type: "GET",
-            dataType: "json",
-            success: function(data){
-                console.log(data);
-                /*for(var i = 0, s = schedules.schedules.length; i < s; i++){
-                    var show = schedules.schedules[i];
-                    $('' + 
-                        '<ul data-role="listview" class="showJson">' + 
-                            '<li>' + show.major + '</li>' + 
-                            '<li>' + show.cName + '</li>' +
-                            '<li>' + show.cSection + '</li>' +
-                            '<li>' + show.topicAndSec + '</li>' +
-                            '<li>' + show.todaysDate + '</li>' +
-                            '<li>' + show.dueDate + '</li>' +
-                            '<li>' + show.numOfWeeks + '</li>' +
-                            '<li>' + show.classOptView + '</li>' +
-                            '<li>' + show.numOfCredits + '</li>' +
-                            '<li>' + show.teachName + '</li>' +
-                            '<li>' + show.teachEmail + '</li>' +
-                            '<li>' + show.teachPhone + '</li>' +
-                            '<li>' + show.option + '</li>' +
-                            '<li>' + show.note + '</li>' + '<br/>' +
-                        '</ul>'
-                        ).appendTo('#showJSON');
-                }*/
-            }
-        });
-        //return false;
-    });
+$(document).on('pageinit', function(){
+	console.log("Now");
+	$.ajax({
+		"url": "_view/schedules",
+		"dataType": "json",
+		"success": function (data) {
+			console.log(data);
+			$.each(data.rows, function(index, schedule){
+				console.log(value);
+				var major = schedule.value.major;
+				var cName = schedule.value.cName;
+				var cSection = schedule.value.cSection;
+				var topicAndSec = schedule.value.topicAndSec;
+				var todaysDate = schedule.value.todaysDate;
+				var dueDate = schedule.value.dueDate;
+				var numOfWeeks = schedule.value.numOfWeeks;
+				var classOptView = schedule.value.classOptView;
+				var numOfCredits = schedule.value.numOfCredits;
+				var teachName = schedule.value.teachName;
+				var teachEmail = schedule.value.teachEmail;
+				var teachPhone = schedule.value.teachPhone;
+				var option = schedule.value.option;
+				var note = schedule.value.note;
+			});
+		}
+	});
 });
--- asdcouch/asdcouch/app/views/schedules/map.js
+++ asdcouch/asdcouch/app/views/schedules/map.js
@@ -1,5 +1,20 @@
 function (doc) {
 	if(doc._id.substr(0, 9) === "schedules") {
-		emit(doc._id);
+		emit(doc._id.substr(9), {
+			"Major": doc.major,
+			"Course Name": doc.cName,
+			"Course Section": doc.cSection,
+			"Topic and Section": doc.topicAndSec,
+			"Today's Date": doc.todaysDate,
+			"Due Date": doc.dueDate,
+			"Number of Weeks": doc.numOfWeeks,
+			"Is the Class Online or OnCampus": doc.classOptView,
+			"Number of Credits": doc.numOfCredits,
+			"Teacher Name": doc.teachName,
+			"Teacher Email": doc.teachEmail,
+			"Teacher Phone Number": doc.teachPhone,
+			"Turn In Option": doc.option,
+			"Note Section": doc.note
+		});
 	}
 };
